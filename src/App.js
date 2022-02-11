24509ceab4884cd63fd93e9387d760846a400bd2
Abinash Dutta <abinash_coder269@mail.com>
2022-02-11 13:32:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h2>React NoCLI</h2>
+        <p>This is the main APP component</p>
+    </div>
+  )
+}
+
+export default App
